039ca7b0925f85158b0e64faf7cff21e756ed595
anel1sa <isafilalobo@hotmail.com>
2025-10-06 16:46:35 -0300

    update workflows, add new version of terraform init

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -10,5 +10,11 @@ terraform {
     
     # Backend S3 para estado remoto
     # Configurado via arquivo backend.tfvars durante terraform init
-    backend "s3" {}
+    backend "s3" {
+        bucket         = "state-sa-east-1-github-aep"
+        key            = "dev/terraform.tfstate"
+        region         = "sa-east-1"
+        use_lockfile  = true
+        encrypt        = true
+    }
 }
